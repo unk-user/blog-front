2e248a3126195a42cf9efd25a8489e3b11a3836d
Hamza <144457566+unk-user@users.noreply.github.com>
2024-03-26 01:15:20 +0000

    add About section

---
+++ src/routes/About.jsx
@@ -0,0 +1,38 @@
+export default function About() {
+  return (
+    <section className="border-4 border-dashed border-primary border-opacity-20 px-6 pb-6 pt-2 text-accent">
+      <h2>Who Am I?</h2>
+      <p>
+        {
+          "I'm Hamza, a mechanical engineering student and a self-taught developer."
+        }
+      </p>
+      <h2>{"What's This Blog?"}</h2>
+      <p>{"It's a place where You can share stories, ideas, and thoughts."}</p>
+      <h2>{"What's Here?"}</h2>
+      <ul>
+        <li>
+          <strong>Recent Posts:</strong> {"Find the latest content in 'blog'."}
+        </li>
+        <li>
+          <strong>Explore More:</strong> {"Go to 'posts' to read more."}
+        </li>
+        <li>
+          <strong>Authors:</strong> Authenticated authors can publish at{' '}
+          {"'/author/posts'"}.
+        </li>
+      </ul>
+      <h2>Connect</h2>
+      <p>Need to reach me? hamzalahmouz01@gmail.com</p>
+      <h2>The Tech Stack</h2>
+      <ul>
+        <li>
+          <strong>Backend:</strong> MongoDB, Express.js, JWT (JSON Web Tokens)
+        </li>
+        <li>
+          <strong>Frontend:</strong> React.js, Tailwind CSS
+        </li>
+      </ul>
+    </section>
+  );
+}
