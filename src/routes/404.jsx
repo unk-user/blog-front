7462acf793115b6811437889d607957be1f0760b
Hamza <144457566+unk-user@users.noreply.github.com>
2024-03-26 01:14:29 +0000

    add 404 page

---
+++ src/routes/404.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+
+export default function ErrorElement() {
+  return (
+    <section className="flex flex-col justify-center items-center h-full px-12">
+      <div className='w-fit bg-blue-300 p-4'>
+        <h1 className="text-2xl font-bold w-fit">404 NOT FOUND</h1>
+        <p>
+          The page you are looking for might have been removed, had its name
+          changed, or is temporarily unavailable.
+        </p>
+        <p>
+          Please check the URL for any spelling mistakes or try navigating back
+          to the <Link className='text-gray-600' to={'/'}>homepage</Link>.
+        </p>
+      </div>
+    </section>
+  );
+}
