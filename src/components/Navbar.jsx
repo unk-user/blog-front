2a31cf8b5c8f240dab9f85c104149f76b38b4547
Hamza <144457566+unk-user@users.noreply.github.com>
2024-03-28 00:27:44 +0000

    update navlinks styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,25 @@
-import { Link } from "react-router-dom"
+import { NavLink } from 'react-router-dom';
 
-export default function Navbar () {
+export default function Navbar() {
   return (
     <header className="w-full h-[60px] flex items-center justify-between mb-10 px-6 text-lg">
-      <nav className="flex gap-6">
-        <Link to='/'>About</Link>
-        <Link to='/Home'>Blog</Link>
-        <Link to='/Posts'>Posts</Link>
+      <nav>
+        <ul className="flex gap-2">
+          <li>
+            <NavLink to="/" className={'w-16 block text-center py-1'}>
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/Blog" className={'w-16 block text-center py-1'}>
+              Blog
+            </NavLink>
+          </li>
+        </ul>
       </nav>
-      <Link className="py-1 px-6 rounded-3xl border-2 border-primary hover:bg-primary hover:text-background transition-colors duration-300">Author</Link>
+      <NavLink className="py-1 px-6 rounded-3xl border-2 border-primary hover:bg-primary hover:text-background transition-colors duration-300">
+        Author
+      </NavLink>
     </header>
-  )
+  );
 }
